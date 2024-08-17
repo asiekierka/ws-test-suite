be4f266de673cb9f0d9f4c690885db1c32fe047a
Adrian Siekierka <kontakt@asie.pl>
2024-08-17 11:20:22 +0200

    mono/eeprom: add 16kbit cartridge test

---
+++ src/mono/eeprom/cartridge-16kbit/main.c
@@ -0,0 +1,2 @@
+#define _HANDLE ws_eeprom_handle_cartridge(10)
+#include "../internal/main.c"
--- src/mono/eeprom/internal/main.c
+++ src/mono/eeprom/internal/main.c
@@ -8,7 +8,7 @@
 
 #ifndef _HANDLE
 #define _HANDLE ws_eeprom_handle_internal()
-// TODO: On IEEPROM, it appears that you cannot erase any word other than @0x000?
+// TODO: On IEEPROM, it appears that you cannot erase any word other than 0x000?
 #define ws_eeprom_erase_word(a,b) ws_eeprom_write_word(a,b,0xffff)
 #define MAX_COMMAND 7
 #define HANDLE_INTERNAL
@@ -23,7 +23,8 @@ ws_tile_t tiles_2bpp[512];
 
 #include "test/pass_fail.h"
 
-static const char __wf_rom msg_init[] = "Init";
+static const char __wf_rom msg_init[] = "Init erase";
+static const char __wf_rom msg_locked_on_start[] = "Locked on start";
 static const char __wf_rom msg_read_ffff[] = "Read erased";
 static const char __wf_rom msg_write_aa55[] = "Write AA55";
 static const char __wf_rom msg_read_aa55[] = "Read  AA55";
@@ -35,23 +36,37 @@ static const char __wf_rom msg_mono_mode[] = "Mono read";
 static const char __wf_rom msg_mwrite_lock[] = "Mono w.lock";
 static const char __wf_rom msg_mwrite_unlock[] = "Mono w.unlock";
 static const char __wf_rom msg_hex2[] = "%04x %04x";
+static const char __wf_rom msg_hex0[] = "%02x";
 static const char __wf_rom msg_invalid_cmds[] = "Invalid cmds";
 static const char __wf_rom msg_write_protect[] = "Write prot.";
+static const char __wf_rom msg_sram_open_bus[] = "SRAM open bus";
 
 // TODO: Test ERAL/WRAL on non-internal EEPROMs
 
 int main(void) {
+    int i = 0;
+
     init_pass_fail();
     cpu_irq_disable();
 
     // init internal EEPROM state
-    text_puts(screen_1, 0, 0, 0, msg_init);
+    text_puts(screen_1, 0, 0, i, msg_init);
     ws_eeprom_handle_t handle = _HANDLE;
+    i++;
+
+#ifndef HANDLE_INTERNAL
+    text_puts(screen_1, 0, 0, i, msg_locked_on_start);
+    uint16_t start_0 = ws_eeprom_read_word(handle, 0x00);
+    ws_eeprom_write_word(handle, 0x00, start_0 ^ 0xFFFF);
+    draw_pass_fail(i, 0, ws_eeprom_read_word(handle, 0x00) == start_0);
+    i++;
+    ws_eeprom_write_unlock(handle);
+#endif
+
     ws_eeprom_erase_word(handle, 0x00);
     ws_eeprom_erase_word(handle, 0x02);
 
     // read erased words (0xffff)
-    int i = 1;
     text_puts(screen_1, 0, 0, i, msg_read_ffff);
     draw_pass_fail(i, 1, ws_eeprom_read_word(handle, 0x00) == 0xffff);
     draw_pass_fail(i, 0, ws_eeprom_read_word(handle, 0x02) == 0xffff);
@@ -75,10 +90,12 @@ int main(void) {
     draw_pass_fail(i, 3, (inportw(handle.port+4)&0x7F) == 0x03);
     i++;
 
+#ifdef HANDLE_INTERNAL
     // read != write
     text_puts(screen_1, 0, 0, i, msg_read_not_write);
     draw_pass_fail(i, 0, read_not_write);
     i++;
+#endif
 
     // erase first word
     text_puts(screen_1, 0, 0, i, msg_erase);
@@ -126,7 +143,6 @@ int main(void) {
     draw_pass_fail(i, 1, inportw(handle.port+4) & 0x80);
     draw_pass_fail(i, 0, ws_eeprom_read_word(handle, 0x60) == value_to_protect);
     i++;
-#endif
 
 #ifndef BOOTFRIEND
     if (ws_system_color_active()) {
@@ -156,8 +172,15 @@ int main(void) {
         i++;
     }
 #endif
+#endif
 
-    text_printf(screen_1, 0, 28-14, 17, msg_hex2, ws_eeprom_read_word(handle, 0x00), ws_eeprom_read_word(handle, 0x02));
+#ifndef HANDLE_INTERNAL
+    // SRAM open bus
+    text_puts(screen_1, 0, 0, i, msg_sram_open_bus);
+    uint8_t open_bus = *((uint8_t __far*) MK_FP(0x1000, 0x8000));
+    text_printf(screen_1, 0, 28-4, i, msg_hex0, open_bus);
+    i++;
+#endif
 
     while(1);
 }
