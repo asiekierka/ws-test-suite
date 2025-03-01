a09eea310e478c2da5024cd05c6a6856895add4a
Adrian Siekierka <kontakt@asie.pl>
2025-03-01 11:36:42 +0100

    80186_interrupts: more exhaustive tests of when exactly IRQ handling occurs

--- src/mono/cpu/80186_interrupts/main.c
+++ src/mono/cpu/80186_interrupts/main.c
@@ -1,6 +1,5 @@
 #include <string.h>
 #include <ws.h>
-#include <ws/display.h>
 #include "text.h"
 
 __attribute__((section(".iramcx_1800")))
@@ -10,13 +9,25 @@ ws_tile_t tiles_2bpp[512];
 
 static const char __wf_rom msg_hex16[] = "%04x";
 
-static const char __wf_rom msg_div_by_zero[] = "/ by zero";
-extern uint16_t do_div_by_zero(void);
-extern char do_div_by_zero_ret;
+#define CONCAT_(a, b) a##b
+#define CONCAT(a, b) CONCAT_(a, b)
+#define IP_STORE_TEST_DEFINE(c, s) \
+    static const char __wf_rom CONCAT(msg_, c)[] = s; \
+    extern uint16_t CONCAT(do_, c)(void); \
+    extern char do_ ## c ## _ret
 
-static const char __wf_rom msg_rep_div_by_zero[] = "REP / by zero";
-extern uint16_t do_rep_div_by_zero(void);
-extern char do_rep_div_by_zero_ret;
+IP_STORE_TEST_DEFINE(div_by_zero, "/ by zero");
+IP_STORE_TEST_DEFINE(rep_div_by_zero, "REP / by zero");
+IP_STORE_TEST_DEFINE(delay_on_sti, "IRQ after STI+NOP");
+IP_STORE_TEST_DEFINE(delay_on_popf, "IRQ after POPF+NOP");
+IP_STORE_TEST_DEFINE(delay_on_es, "IRQ after ES:LODSW");
+IP_STORE_TEST_DEFINE(delay_on_lock, "IRQ after LOCK:LODSW");
+IP_STORE_TEST_DEFINE(no_delay_sti_sti, "IRQ after STI+STI");
+IP_STORE_TEST_DEFINE(test_delay_sti_popf, "IRQ after STI+POPF");
+IP_STORE_TEST_DEFINE(delay_on_pop_ss, "IRQ after POP SS+NOP");
+IP_STORE_TEST_DEFINE(delay_on_mov_ss, "IRQ after MOV SS,+NOP");
+IP_STORE_TEST_DEFINE(no_delay_mov_from_ss, "IRQ after MOV ,SS");
+// TODO: test IRET behaviour
 
 #include "test/pass_fail.h"
 
@@ -24,17 +35,31 @@ int main(void) {
     init_pass_fail();
     cpu_irq_disable();
 
+    // Enable serial port (this is used for the UART send ready IRQ)
+    ws_serial_open(SERIAL_BAUD_9600);
+    outportb(IO_HWINT_VECTOR, 0x08);
+
     uint8_t i = 0;
+    uint16_t result;
 
-    text_puts(screen_1, 0, 0, i, msg_div_by_zero);
-    uint16_t result = do_div_by_zero();
-    text_printf(screen_1, 0, 28-5, i, msg_hex16, result);
-    draw_pass_fail(i++, 0, result == (uint16_t) &do_div_by_zero_ret);
+#define IP_STORE_TEST_CALL(c) \
+    text_puts(screen_1, 0, 0, i, CONCAT(msg_, c)); \
+    result = CONCAT(do_, c)(); \
+    text_printf(screen_1, 0, 28-5, i, msg_hex16, result); \
+    draw_pass_fail(i++, 0, result == (uint16_t) &CONCAT(do_, CONCAT(c, _ret)))
 
-    text_puts(screen_1, 0, 0, i, msg_rep_div_by_zero);
-    result = do_rep_div_by_zero();
-    text_printf(screen_1, 0, 28-5, i, msg_hex16, result);
-    draw_pass_fail(i++, 0, result == (uint16_t) &do_rep_div_by_zero_ret);
+    IP_STORE_TEST_CALL(div_by_zero);
+    IP_STORE_TEST_CALL(rep_div_by_zero);
+    IP_STORE_TEST_CALL(delay_on_sti);
+    IP_STORE_TEST_CALL(delay_on_popf);
+    IP_STORE_TEST_CALL(delay_on_es);
+    IP_STORE_TEST_CALL(delay_on_lock);
+    IP_STORE_TEST_CALL(no_delay_sti_sti);
+    IP_STORE_TEST_CALL(test_delay_sti_popf);
+    IP_STORE_TEST_CALL(delay_on_pop_ss);
+    IP_STORE_TEST_CALL(delay_on_mov_ss);
+    IP_STORE_TEST_CALL(no_delay_mov_from_ss);
 
+    ws_serial_close();
     while(1);
 }
--- src/mono/cpu/80186_interrupts/tests.s
+++ src/mono/cpu/80186_interrupts/tests.s
@@ -7,33 +7,67 @@
 
 #define OVERRIDE_REP 0xF3
 
-    .global do_store_ip_on_irq
-do_store_ip_on_irq:
+    .macro do_preamble
+    push ds
+    push es
+    push si
+    push di
+    .endm
+
+    .macro do_postamble
+    pop di
+    pop si
+    pop es
+    pop ds
+    WF_PLATFORM_RET
+    .endm
+
+    .macro set_hwint_enable v
+    mov al, \v
+    out IO_HWINT_ENABLE, al
+    .endm
+
+
+    // AX = offset
+    // DX = IRQ vector
+set_irq_vector:
+    // BP = IRQ vector * 4
+    push bp
+    mov bp, dx
+    shl bp, 2
+
+    ss mov word ptr [bp], ax
+    mov ax, cs
+    ss mov word ptr [bp + 2], ax
+
+    pop bp
+    ret
+
+irq_handler_store_ip_on_irq:
     push bp
     mov bp, sp
     push ax
     mov ax, [bp + 2]
     ss mov [ip_on_irq], ax
-    mov al, HWINT_HBLANK_TIMER
+    mov al, 0x00
+    out IO_HWINT_ENABLE, al
+    mov al, 0xFF
     out IO_HWINT_ACK, al
     pop ax
     pop bp
     iret
 
+
     .global do_div_by_zero
 do_div_by_zero:
-    push ds
-    push es
-    push si
-    push di
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 0
+    call set_irq_vector
 
     mov ax, 1024
     mov bl, 0
-
-    mov ax, offset do_store_ip_on_irq
-    ss mov word ptr [0 * 4], ax
-    mov ax, cs
-    ss mov word ptr [0 * 4 + 2], ax
 
     pushf
     sti
@@ -47,28 +81,20 @@ do_div_by_zero_ret:
 
     ss mov ax, [ip_on_irq]
 
-    pop di
-    pop si
-    pop es
-    pop ds
+    do_postamble
 
-    WF_PLATFORM_RET
 
     .global do_rep_div_by_zero
 do_rep_div_by_zero:
-    push ds
-    push es
-    push si
-    push di
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 0
+    call set_irq_vector
 
     mov ax, 1024
     mov bl, 0
     mov cx, 1
-
-    mov ax, offset do_store_ip_on_irq
-    ss mov word ptr [0 * 4], ax
-    mov ax, cs
-    ss mov word ptr [0 * 4 + 2], ax
 
     pushf
     sti
@@ -83,12 +109,257 @@ do_rep_div_by_zero_ret:
 
     ss mov ax, [ip_on_irq]
 
-    pop di
-    pop si
-    pop es
-    pop ds
+    do_postamble
 
-    WF_PLATFORM_RET
+
+    .global do_delay_on_sti
+do_delay_on_sti:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    pushf
+    sti
+    nop
+
+    .global do_delay_on_sti_ret
+do_delay_on_sti_ret:
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
+
+    .global do_delay_on_popf
+do_delay_on_popf:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    pushf
+    
+    // set interrupt in pushed FLAGS
+    pushf
+    pop ax
+    or ah, 0x02
+    push ax
+    nop
+    nop
+
+    popf
+    nop
+
+    .global do_delay_on_popf_ret
+do_delay_on_popf_ret:
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
+
+    .global do_delay_on_es
+do_delay_on_es:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    pushf
+    sti
+    es lodsw
+
+    .global do_delay_on_es_ret
+do_delay_on_es_ret:
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
+
+    .global do_delay_on_lock
+do_delay_on_lock:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    pushf
+    sti
+    lock
+    lodsw
+
+    .global do_delay_on_lock_ret
+do_delay_on_lock_ret:
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
+
+    .global do_no_delay_sti_sti
+do_no_delay_sti_sti:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    pushf
+    sti
+    sti
+
+    .global do_no_delay_sti_sti_ret
+do_no_delay_sti_sti_ret:
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
+
+    .global do_test_delay_sti_popf
+do_test_delay_sti_popf:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    pushf
+    
+    // set interrupt in pushed FLAGS
+    pushf
+    pop ax
+    or ah, 0x02
+    push ax
+    nop
+    nop
+
+    sti
+    popf
+
+    .global do_test_delay_sti_popf_ret
+do_test_delay_sti_popf_ret:
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
+
+    .global do_delay_on_pop_ss
+do_delay_on_pop_ss:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    pushf
+    push ss
+    sti
+    pop ss
+    nop
+
+    .global do_delay_on_pop_ss_ret
+do_delay_on_pop_ss_ret:
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
+
+    .global do_delay_on_mov_ss
+do_delay_on_mov_ss:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    xor ax, ax
+    pushf
+    sti
+    mov ss, ax
+    nop
+
+    .global do_delay_on_mov_ss_ret
+do_delay_on_mov_ss_ret:
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
+
+    .global do_no_delay_mov_from_ss
+do_no_delay_mov_from_ss:
+    do_preamble
+
+    mov ax, offset irq_handler_store_ip_on_irq
+    mov dx, 8
+    call set_irq_vector
+
+    set_hwint_enable 0x01
+
+    pushf
+    sti
+    mov ax, ss
+
+    .global do_no_delay_mov_from_ss_ret
+do_no_delay_mov_from_ss_ret:
+    nop
+    nop
+
+    popf
+
+    set_hwint_enable 0x00
+    ss mov ax, [ip_on_irq]
+    do_postamble
+
 
     .section ".bss"
 ip_on_irq:
